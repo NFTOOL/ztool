7a90dd2079977f11f7ab489fa9958aeb76e27c8b
NFTOOL <105815715+NFTOOL@users.noreply.github.com>
2022-05-21 16:34:23 +0800

    Create remove.sh

---
+++ remove.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if  command -v docker &> /dev/null; then
+echo "Start docker"
+sudo systemctl start docker
+sudo service docker start
+echo "Stop running instance"
+sudo docker ps -q --filter name=ztool | xargs -r sudo docker stop
+echo "Remove old instance"
+sudo docker image rm -f nft9/ztool:stable 2> /dev/null || true
+fi
