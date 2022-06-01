a0d4c496167910c8dd010077e1bb58df74df4a87
NFTOOL <105815715+NFTOOL@users.noreply.github.com>
2022-06-02 00:27:58 +0800

    Update remove.sh

--- remove.sh
+++ remove.sh
@@ -7,5 +7,6 @@ echo "Stop running instance"
 sudo docker ps -q --filter name=ztool | xargs -r sudo docker stop
 echo "Remove old instance"
 sudo docker image rm -f nft9/ztool:stable 2> /dev/null || true
+sudo docker system prune -f
 fi
 echo "Done"
