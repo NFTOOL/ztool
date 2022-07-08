b296005c92cfd10a8b03a6265e14f1e088f4e427
NFTOOL <105815715+NFTOOL@users.noreply.github.com>
2022-07-09 05:19:32 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -18,4 +18,4 @@ echo "Start tool"
 ulimit -n 99999
 sudo mkdir -p /mnt/ztool/profiles
 sudo chmod 777 /mnt/ztool/profiles
-sudo docker run -v /mnt/ztool/profiles:/home/ztooluser/profiles:Z -v /dev/shm:/dev/shm -p 80:8686 --rm --name ztool --dns="1.1.1.1" --dns="1.0.0.1" --cap-add=SYS_ADMIN nft9/ztool:stable
+sudo docker run --shm-size=10gb -v /mnt/ztool/profiles:/home/ztooluser/profiles:Z -v /dev/shm:/dev/shm -p 80:8686 --rm --name ztool --dns="1.1.1.1" --dns="1.0.0.1" --cap-add=SYS_ADMIN nft9/ztool:stable
