67fe1218fc7b1aec230e1d403b4a4190abd4b6c0
NFTOOL <105815715+NFTOOL@users.noreply.github.com>
2022-07-14 03:27:31 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -25,18 +25,25 @@ vm.overcommit_memory=1
 
 fs.file-max=999999
 fs.nr_open=999999
+
 net.ipv4.ip_local_port_range=1024 65000
 net.ipv4.tcp_keepalive_time=60
 net.ipv4.tcp_keepalive_probes=3
-net.ipv4.tcp_keepalive_intvl=90
+net.ipv4.tcp_keepalive_intvl=20
 net.ipv4.tcp_max_syn_backlog=100000
+net.netfilter.nf_conntrack_tcp_timeout_established =600
+net.netfilter.nf_conntrack_tcp_timeout_time_wait=10
+net.nf_conntrack_max = 655360
 net.core.somaxconn = 100000
 net.core.netdev_max_backlog = 100000
+
 # Timeout in seconds to close client connections in
 # TIME_WAIT after receiving FIN packet.
 net.ipv4.tcp_fin_timeout = 10
+
 # Disable SYN cookie flood protection
 net.ipv4.tcp_syncookies = 0
+net.ipv4.tcp_timestsmps= 0
 
 # 16MB per socket - which sounds like a lot,
 # but will virtually never consume that much.
@@ -44,18 +51,23 @@ net.core.rmem_max=16777216
 net.core.wmem_max=16777216
 
 # Various network tunables
-net.ipv4.tcp_max_syn_backlog=20480
+net.ipv4.tcp_max_syn_backlog=65535
 net.ipv4.tcp_max_tw_buckets=400000
 net.ipv4.tcp_no_metrics_save=1
 net.ipv4.tcp_rmem=4096 87380 16777216
 net.ipv4.tcp_syn_retries=2
 net.ipv4.tcp_synack_retries=2
-net.ipv4.tcp_wmem=4096 65536 16777216
+net.ipv4.tcp_tw_reuse= 1
+net.ipv4.tcp_tw_recycle = 1
+
+net.ipv4.tcp_wmem=8192 436600 873200
+net.ipv4.tcp_rmem = 32768 436600 873200
 
 # ARP cache settings for a highly loaded docker swarm
 net.ipv4.neigh.default.gc_thresh1=8096
 net.ipv4.neigh.default.gc_thresh2=12288
 net.ipv4.neigh.default.gc_thresh3=16384
+
 # monitor file system events
 fs.inotify.max_user_instances=8192
 fs.inotify.max_user_watches=1048576
@@ -82,11 +94,18 @@ sudo docker run \
   --sysctl net.ipv4.ip_local_port_range="1024 65535" \
   --sysctl net.ipv4.tcp_keepalive_time="60" \
   --sysctl net.ipv4.tcp_keepalive_probes="3" \
-  --sysctl net.ipv4.tcp_keepalive_intvl="90" \
+  --sysctl net.ipv4.tcp_keepalive_intvl="20" \
   --sysctl net.ipv4.tcp_max_syn_backlog="100000" \
   --sysctl net.core.somaxconn="100000" \
   --sysctl net.ipv4.tcp_fin_timeout=10 \
+  --sysctl net.netfilter.nf_conntrack_tcp_timeout_established=600 \
+  --sysctl net.netfilter.nf_conntrack_tcp_timeout_time_wait=10 \
+  --sysctl net.nf_conntrack_max=655360 \
   --sysctl net.ipv4.tcp_syncookies=0 \
+  --sysctl net.ipv4.tcp_tw_reuse= 1 \
+  --sysctl net.ipv4.tcp_tw_recycle = 1 \
+  --sysctl net.ipv4.tcp_syn_retries=2 \
+  --sysctl net.ipv4.tcp_synack_retries=2 \
   --shm-size=10gb \
   -v /mnt/ztool/profiles:/home/ztooluser/profiles:Z \
   -v /dev/shm:/dev/shm \
