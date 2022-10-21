93dd006fa83d34f5925ffcac6c43b91e80aab099
NFTOOL <105815715+NFTOOL@users.noreply.github.com>
2022-10-21 08:38:23 +0800

    Delete stop.sh

--- stop.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if  command -v docker &> /dev/null; then
-echo "Start docker"
-sudo systemctl start docker
-sudo service docker start
-echo "Stop running instance"
-sudo docker ps -q --filter name=ztool | xargs -r sudo docker stop
-fi
-echo "Done"
